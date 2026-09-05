316d760b0ecd13f399c0310568c5c72f1b0e5246
besS0LI <150979594+besS0LI@users.noreply.github.com>
2026-09-05 13:14:33 +0300

    Улучшить headless Chrome для Debian и безопасно освобождать WebDriver

--- services/browser_service.py
+++ services/browser_service.py
@@ -7,9 +7,14 @@ class BrowserService:
         self.driver = None
 
     def start(self):
+        if self.driver is not None:
+            return
+
         options = webdriver.ChromeOptions()
         options.add_argument("--headless=new")
         options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--ignore-certificate-errors")
         options.add_argument("--ignore-ssl-errors=yes")
@@ -18,5 +23,7 @@ class BrowserService:
 
     def stop(self):
         if self.driver:
-            self.driver.quit()
-            self.driver = None
+            try:
+                self.driver.quit()
+            finally:
+                self.driver = None
